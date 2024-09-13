e582f8ded507619c3ea1387bab5b6d86f3983797
Ranveer225 <singr12@farmingdale.edu>
2024-09-13 17:29:00 -0400

    Add in text fields for GUI

--- src/main/java/com/example/lab2/HelloController.java
+++ src/main/java/com/example/lab2/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.lab2;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField annualInterestRate;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField loanAmount;
+
+    @FXML
+    private TextField numberOfYears;
+
+    @FXML
+    private TextField monthlyPayment;
+
+    @FXML
+    private TextField totalPayment;
+
+
     }
-}
